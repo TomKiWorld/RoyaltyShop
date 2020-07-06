9316d17dde62e18e286d6084e5196dfa7bbd09c0
TomKiDev <tomkidev@gmail.com>
2020-07-06 12:32:14 +0200

    Testing for Heroku

--- server.js
+++ server.js
@@ -15,10 +15,6 @@ app.use(cors());
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
-
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-  })
 }
 
 app.listen(port, error => {
