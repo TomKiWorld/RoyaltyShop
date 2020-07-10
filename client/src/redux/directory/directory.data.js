25a5cd94330da98a221cd568fbfe8873fa1bcebc
TomKiDev <tomkidev@gmail.com>
2020-07-10 17:48:03 +0200

    Load jpg instead of png

--- client/src/redux/directory/directory.data.js
+++ client/src/redux/directory/directory.data.js
@@ -2,32 +2,32 @@ const DIRECTORY_DATA = {
   sections: [
     {
       title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.jpg',
       id: 1,
       linkUrl: 'shop/hats'
     },
     {
       title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.jpg',
       id: 2,
       linkUrl: 'shop/jackets'
     },
     {
       title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.jpg',
       id: 3,
       linkUrl: 'shop/sneakers'
     },
     {
       title: 'women',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.jpg',
       size: 'large',
       id: 4,
       linkUrl: 'shop/women'
     },
     {
       title: 'men',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.jpg',
       size: 'large',
       id: 5,
       linkUrl: 'shop/men'
